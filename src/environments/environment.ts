c6135b4382f7f4c5cc89ce8ccfc92e540d398f99
Gianfranco Giordano Padrón <gianfrancogiordano@gmail.com>
2021-02-21 21:27:26 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDT890rPGInsjAcCU1jwCiTpTc7PaOPzMU',
+    apiKey: '',
     authDomain: 'alimentos-colombeia.firebaseapp.com',
-    databaseURL: 'https://alimentos-colombeia.firebaseio.com',
+    databaseURL: '',
     projectId: 'alimentos-colombeia',
     storageBucket: 'alimentos-colombeia.appspot.com',
     messagingSenderId: '338093238993',
-    appId: '1:338093238993:web:0ebfab5f014834e6f7f3f0',
+    appId: '',
     measurementId: 'G-1SPWMT7MFL'
   },
   urlServicios: 'https://app.alimentoscolombeia.com/'
